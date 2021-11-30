598e165a6f7d71f1857c558f513002abe5b0e83a
foglabs <foglaboratories@gmail.com>
2021-11-30 10:15:25 -0500

    'remove active storage disable'

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-# disable so we can disable only active_storage
-# require 'rails/all'
-require 'rails'
-require "active_job/railtie"
-require "active_record/railtie"
-#require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
-
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
