30081987a1153ca239b57fb131fdd7238bb19d9d
foglabs <foglaboratories@gmail.com>
2021-12-03 14:51:36 -0500

    'move devise_token... route'

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   resources :transcripts, only: [:index, :show, :update]
   resources :collections, only: [:index, :show]
 
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-
   match 'page/:id' => 'default#index', :via => [:get]
   match 'dashboard' => 'default#index', :via => [:get]
   match 'transcript_lines/:id/resolve' => 'transcript_lines#resolve', :via => [:post]
@@ -37,5 +35,6 @@ Rails.application.routes.draw do
   end
   match 'moderator' => 'admin/flags#index', :via => [:get], :as => :moderator
 
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root :to => 'default#index'
 end
