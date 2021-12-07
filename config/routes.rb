7571e0e54d82657ce78670f860b82a85a867fb3f
foglabs <foglaboratories@gmail.com>
2021-12-07 16:21:30 -0500

    'follow overrides example'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: { :omniauth_callbacks => "users/omniauth_callbacks"}
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: { :omniauth_callbacks => "overrides/omniauth_callbacks"}
 
   resources :flags, only: [:index, :show, :create]
   resources :transcript_speaker_edits, only: [:create]
