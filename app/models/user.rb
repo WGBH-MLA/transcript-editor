a10af3ee0a491502a6172ac6242665787b557cd5
foglabs <foglaboratories@gmail.com>
2021-12-08 09:27:44 -0500

    'different line for omniauthable'

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
 
   # Include default devise modules.
   devise :database_authenticatable, :rememberable, :trackable, :validatable
-  devise :omniauthable, omniauth_providers: [:google_oauth2]
   include DeviseTokenAuth::Concerns::User
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   belongs_to :user_role
 
