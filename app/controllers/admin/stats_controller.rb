fcaedb57ad99d0dba9d4ef6761b4852cbb0bc233
foglabs <foglaboratories@gmail.com>
2021-12-20 12:14:59 -0500

    'stupid user test'

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,10 +1,12 @@
 class Admin::StatsController < ApplicationController
   include ActionController::MimeResponds
-  # before_action :authenticate_moderator!
+  before_action :authenticate_moderator!
 
   # GET /admin
   # GET /admin.json
   def index
+    current_user = User.all.first
+
     respond_to do |format|
       format.html {
         render :file => "public/#{ENV['PROJECT_ID']}/index.html"
