ac5ed85becfa73259e8787dbe887de42c09c3ea1
foglabs <foglaboratories@gmail.com>
2021-12-21 16:06:59 -0500

    'but what about tthis'

--- app/controllers/admin/flags_controller.rb
+++ app/controllers/admin/flags_controller.rb
@@ -1,12 +1,15 @@
 class Admin::FlagsController < ApplicationController
   include ActionController::MimeResponds
 
-  before_action :authenticate_moderator!
+  # before_action :authenticate_moderator!
 
   # GET /admin/flags
   # GET /admin/flags.json
   # GET /moderator
   def index
+    puts "BUT WHAT ABOUT THIS!"
+    puts current_user.inspect
+
     respond_to do |format|
       format.html {
         render :file => "public/#{ENV['PROJECT_ID']}/admin.html"
