688bfc0f32b6d9f999b93f6e11bf4f365d4edb03
foglabs <foglaboratories@gmail.com>
2022-01-11 13:49:29 -0500

    'log headers in defcon'

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,10 +1,13 @@
 class DefaultController < ApplicationController
   def index
     # render the project set in environment
+    puts "FULL HEADERS DEFAULT"
+    puts request.headers
     render :file => Rails.root + "public/index.html"
   end
 
   def admin
+    puts "FULL HEADERS ADMIN"
     render :file => Rails.root + "public/admin.html"
   end
 end
