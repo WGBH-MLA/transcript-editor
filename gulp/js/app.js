b774458c9aa313196c27f755eb10e1153717876b
foglabs <foglaboratories@gmail.com>
2022-01-11 15:38:52 -0500

    'squash to one'

--- gulp/js/app.js
+++ gulp/js/app.js
@@ -1,7 +1,7 @@
 window.API_URL = PROJECT.apiUrl || window.location.protocol + '//' + window.location.hostname;
 if (window.location.port && !PROJECT.apiUrl) window.API_URL += ':' + window.location.port
 
-window.DEBUG = true;
+window.DEBUG = false;
 
 window.app = {
   models: {},
@@ -13,8 +13,22 @@ window.app = {
     var auth_provider_paths = _.object(_.map(PROJECT.authProviders, function(provider) { return [provider.name, provider.path]; }));
     $.auth.configure({
       apiUrl: API_URL,
-      authProviderPaths: auth_provider_paths
+      authProviderPaths: auth_provider_paths,
     });
+
+    // explicitly add auth headers toe very request so that admin data api (*.json) requests use auth correctly
+    $.ajaxSetup({ beforeSend: function(xhr) {
+
+      if(typeof $.cookie().authHeaders !== 'undefined'){
+        let cookieData = JSON.parse($.cookie().authHeaders)
+        xhr.setRequestHeader('access-token', cookieData['access-token'])
+        xhr.setRequestHeader('client', cookieData['client'])
+        xhr.setRequestHeader('token-type', cookieData['token-type'])
+        xhr.setRequestHeader('expiry', cookieData['expiry'])
+        xhr.setRequestHeader('uid', cookieData['uid'])
+      }
+
+    }});
 
     // Debug
     DEBUG && console.log("Project", PROJECT);
